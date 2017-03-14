d3873787ca016c6970c32bb0f866b9dd30a17f0a
Damjan Miklic <damjan.miklic@fer.hr>
2017-03-14 14:12:45 +0100

    Ignoring .mat files. Update dohvati.

--- simulink/dohvati.m
+++ simulink/dohvati.m
@@ -1,28 +1,22 @@
+% DATA = DOHVATI Dohvatipodatke i ucitaj ih u varijablu DATA
+%   Sljedece naredbe naredbom scp prebacuju matrice podatke s Taraget
+%   računala u ../data direktorij nadzornog racunala i ucitavaju ih u
+%   zadanu workspace varijablu kao strukturu
+
 function data= dohvati()
-%slijedeće naredbe prebacuju matrice podataka s Taraget računala na
-%nadzorno pomoću naredbe SCP te ih spremaju u
-%working_directory/simulink/Data
 
-global working_directory
-%path=[working_directory,'/simulink/Data'];
-path = 'Data'
-unix(sprintf('scp root@161.53.68.185:q.mat %s/q.mat',path))
-unix(sprintf('scp root@161.53.68.185:dq.mat %s/dq.mat',path));
-unix(sprintf('scp root@161.53.68.185:u.mat %s/u.mat',path))
-unix(sprintf('scp root@161.53.68.185:q_ref.mat %s/q_ref.mat',path))
-unix(sprintf('scp root@161.53.68.185:dq_ref.mat %s/dq_ref.mat',path));
+data_path = '../data';
+unix(sprintf('scp root@target:q.mat %s/q.mat', data_path))
+unix(sprintf('scp root@target:dq.mat %s/dq.mat', data_path));
+unix(sprintf('scp root@target:u.mat %s/u.mat',data_path))
+unix(sprintf('scp root@target:q_ref.mat %s/q_ref.mat', data_path))
+unix(sprintf('scp root@target:dq_ref.mat %s/dq_ref.mat', data_path));
 
-% load (sprintf('%s/q.mat',path));
-% load (sprintf('%s/dq.mat',path));
-% load (sprintf('%s/u.mat',path));
-% load (sprintf('%s/q_ref.mat',path));
-% load (sprintf('%s/dq_ref.mat',path));
-
-load ('Data/q.mat');
-load ('Data/dq.mat');
-load ('Data/u.mat');
-load ('Data/q_ref.mat');
-load ('Data/dq_ref.mat');
+load(sprintf('%s/q.mat',data_path));
+load(sprintf('%s/dq.mat', data_path));
+load(sprintf('%s/u.mat', data_path));
+load(sprintf('%s/q_ref.mat', data_path));
+load(sprintf('%s/dq_ref.mat', data_path));
 
 data.t = q(1,:)';
 data.q1 = q(2,:)';
